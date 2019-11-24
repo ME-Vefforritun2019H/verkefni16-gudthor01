0aeccd26c5212b6b6133fd4c41f89fd3c9466f30
gudthor01 <gudthor01@hotmail.com>
2019-11-24 13:45:29 +0000

    BÚINN

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+let table = 3;
+let operator = "addition";
+let i = 1;
+let msg = "" ;
+
+if (operator==="addition"){
+    while (i<11) {
+        msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+        i++;
+    }
+}else{
+    while (i<11){
+        msg +=i + ' x ' + table + ' = ' + (i * table) + '<br />';
+        i++;
+    }
+}
+
+let el = document.getElementById("blackboard");
+el.innerHTML = msg;
